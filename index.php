b075687505f69ef501049b6edab5caa1c5070dfb
Györkös Balázs <balazs.gyorkos@w5labs.com>
2020-06-26 14:09:39 +0200

    work of 5perc

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Lesz-e vajon a koronavírusnak második hulláma?</title>
+    <link rel="stylesheet" href="style.css">
+    <style type="text/css">
+        html, body {
+            height: 100%;
+        }
+
+        body {
+            background: #00b1b3;
+            font-family: sans-serif;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        h1 {
+            font-size: 4em;
+            color: aliceblue;
+        }
+    </style>
+</head>
+<body>
+
+<h1>Még nem tudni.</h1>
+
+</body>
+</html>
